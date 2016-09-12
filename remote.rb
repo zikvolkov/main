245a3445151f9aee1bb04ef458b5b12505133595
Serj Volkov <zikvolkov@gmail.com>
2016-09-12 13:55:08 +0300

    Exercise

---
+++ remote.rb
@@ -0,0 +1,25 @@
+require 'watir-webdriver'
+require 'nokogiri'
+require 'pry'
+
+browser = Watir::Browser.new
+browser.goto('file:///home/zik/ruby/web.html')
+
+page = Nokogiri::HTML(browser.html)
+
+data = []
+
+page.css('.owwb-cs-slide-list-account-item').each do |i|
+  account = i.css('.contract-number').text
+  balance = i.css('.owwb-cs-slide-list-amount-value').text
+  currency = i.css('.owwb-cs-slide-list-amount-currency').text
+  nature = i.css('.owwb-cs-slide-list-properties-list-property-value').text.gsub('[MDL]', '').strip
+  
+  data.push(  
+    name: account,
+    balance: balance,
+    currency: currency ,
+    nature: nature
+    )  
+end  
+binding.pry
